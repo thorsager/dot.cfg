817bd46eb9c9195f7b1b482b557f96afcba842b2
Michael Thorsager <thorsager@gmail.com>
2023-01-08 01:14:18 +0100

    stuff

--- .config/nvim/lua/prime/packer.lua
+++ .config/nvim/lua/prime/packer.lua
@@ -21,6 +21,13 @@ return require('packer').startup(function(use)
 
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('nvim-treesitter/playground')
+    use('nvim-treesitter/nvim-treesitter-context')
+
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
 
@@ -58,5 +65,4 @@ return require('packer').startup(function(use)
         requires = {"mfussenegger/nvim-dap"}
     }
 
-    use('nvim-treesitter/nvim-treesitter-context')
 end)
